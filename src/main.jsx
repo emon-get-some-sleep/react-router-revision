8a3bc8b1afed1fa23e22cae4ff2aa2abf4c07eee
emonh0que <you@example.com>
2023-04-06 20:48:25 +0600

    added main

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,18 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import Home from './component/Home/Home';
+import Main from './component/Layout/Main';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   }
 ])
 
