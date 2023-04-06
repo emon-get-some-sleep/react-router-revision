de63c5ae6bbe76eeee363d505597ffaa0e92c011
emonh0que <you@example.com>
2023-04-06 22:40:49 +0600

    added readme addme

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const tshirts = useLoaderData();
-    console.log(tshirts);
+    console.log(typeof tshirts);
     return (
         <div>
             <h2>This is home {tshirts.length}</h2>
